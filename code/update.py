70a08cdf846dda858480023387c4208b30e7c7b7
tsukutarou <tsukutarou.blog@gmail.com>
2022-04-25 12:53:02 +0900

    commit

---
+++ code/update.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def update():
+
+     URL = "https://www.atptour.com/en/players/jiri-vesely/v708/overview"
+
+
+if __name__=="__main__":
+
+     update()
