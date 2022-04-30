9e7c57c904bb43ff4b54195341fd66932d5542da
tsukutarou <tsukutarou.blog@gmail.com>
2022-04-30 11:26:20 +0900

    commit

--- code/update.py
+++ code/update.py
@@ -1,5 +1,5 @@
-import requests
 import os
+import re
 import time
 
 from selenium import webdriver
@@ -9,9 +9,6 @@ from bs4 import BeautifulSoup
 def update():
 
       url = "https://www.atptour.com/en/players/jiri-vesely/v708/overview"
-      home_url = "https://www.atptour.com/en/players"
-      header = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-                "referer":home_url}
 
       #ファイルのある場所に移動
       os.chdir(os.path.dirname(__file__))
@@ -19,7 +16,8 @@ def update():
       #ドライバーの設定 
       options = webdriver.ChromeOptions()
       options.add_experimental_option('excludeSwitches', ['enable-logging'])
-      #options.add_argument('--headless')
+      options.add_argument('--headless')
+      options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36')
       driver = webdriver.Chrome('..\\chromedriver',options=options) 
       
       try:
@@ -33,20 +31,14 @@ def update():
       finally:
            driver.close()
 
+      ranking = [td for td in soup.find_all("td") if len(td.find_all("div",text=re.compile('Rank')))!=0][0]
+      singles = ranking.find_all("div")[0].get("data-singles")
+      doubles = ranking.find_all("div")[0].get("data-doubles")
+      print(singles,doubles)
 
-      s = requests.Session()
-      res = s.get(url=url, headers=header)
-      cookies = dict(res.cookies)
+      week = ([h2.string.replace(' ','').replace('\n','') for h2 in soup.find_all("h2",attrs = {"class":"module-title"}) if "As" in h2.string][0])[-10:]
+      print(week,len(week))
 
-      print(cookies)
-
-      #res = requests.get(url=url, headers=header)
-
-      print(res)
-
-      print(soup)
-      with open("test.html", mode="w", encoding="UTF-8") as f:
-           f.write(str(soup))
 
 
 if __name__=="__main__":
